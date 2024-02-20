825a5a6fa8fc7a59ad17057cd094a4e4da8363fc
tuan2809nh@gmail.com <tuan2809nh@gmail.com>
2024-02-20 16:54:50 +0000

    Edited Form1 and custom package dependencies

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 
+import ipaddress
+
 class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
