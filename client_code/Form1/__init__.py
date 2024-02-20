651431e1522e9b07003b2c4d68ecfd69144718dc
tuan2809nh@gmail.com <tuan2809nh@gmail.com>
2024-02-20 17:21:15 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
-import ipaddress
+from anvil import ipaddress
 
 class Form1(Form1Template):
     def __init__(self, **properties):
