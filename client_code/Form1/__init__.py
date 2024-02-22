2291289fae6a2942f2ca724973c2db0bf25e1dd3
tuan2809nh@gmail.com <tuan2809nh@gmail.com>
2024-02-22 15:38:50 +0000

    Edited Form1 and custom package dependencies

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
-
+import ipaddress
 class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
@@ -10,20 +10,20 @@ class Form1(Form1Template):
     def subnet_calculation(self, ip_address, subnet_mask):
         try:
             # Tạo đối tượng IPv4Network từ địa chỉ IP và subnet mask
-            network = ipaddress.IPv4Network(f'{ip_address}/{subnet_mask}', strict=False)
+         network = ipaddress.IPv4Network(f'{ip_address}/{subnet_mask}', strict=False)
 
             # Trả về thông tin về mạng và subnet mask
-            return f"Địa chỉ IP: {network.network_address}, Subnet Mask: {network.netmask}"
+         return f"Địa chỉ IP: {network.network_address}, Subnet Mask: {network.netmask}"
 
         except ValueError:
             return "Địa chỉ IP hoặc subnet mask không hợp lệ."
 
     def button_1_click(self, **event_args):
-        ip_address = self.text_box_ip_address.text
-        subnet_mask = self.text_box_subnet_mask.text
-
-        # Gọi hàm subnet_calculation để tính toán và hiển thị thông tin mạng con
-        result = self.subnet_calculation(ip_address, subnet_mask)
-
-        # Hiển thị kết quả trong textbox_result
-        self.textbox_result.text = result
+      ip_address = self.text_box_ip_address.text
+      subnet_mask = self.text_box_subnet_mask.text
+  
+          # Gọi hàm subnet_calculation để tính toán và hiển thị thông tin mạng con
+      result = self.subnet_calculation(ip_address, subnet_mask)
+  
+          # Hiển thị kết quả trong textbox_result
+      self.textbox_result.text = result
